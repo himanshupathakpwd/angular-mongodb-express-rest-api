12d37634a5bc4cf6a07f60e1fe5f7f7dbfc8d9e1
Himanshu Pathak <himanshupathakpwd@gmail.com>
2017-01-16 18:41:29 +0530

    Formatting

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,25 +1,31 @@
 (function() {
-    angular.module('myApp', ['ui.router', 'ngResource']);
+  angular.module('myApp', ['ui.router', 'ngResource']);
 
-    angular.module('myApp').config(appConfig).run(appRun);
+  angular.module('myApp').config(appConfig).run(appRun);
 
-    appConfig.$inject = ['$stateProvider', '$httpProvider', '$urlRouterProvider'];
-    function appConfig($stateProvider, $httpProvider, $urlRouterProvider) {
-        // Used for express to detect that this is an AJAX request
-        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-        // Send to home page for any unmatched URL.
-        $urlRouterProvider.otherwise("/home");
-        $stateProvider.state('home', {
-            url: '/home',
-            templateUrl: '/templates/home'
-        });
-        $stateProvider.state('about', {
-            url: '/about',
-            templateUrl: '/templates/about'
-        });
-    }
+  appConfig.$inject = ['$stateProvider', '$httpProvider', '$urlRouterProvider'];
+  function appConfig($stateProvider, $httpProvider, $urlRouterProvider) {
+    // Used for express to detect that this is an AJAX request
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    // Send to home page for any unmatched URL.
+    $urlRouterProvider.otherwise("/home");
+    var states = [
+      {
+        name: 'home',
+        url: '/home',
+        templateUrl: '/templates/home'
+      }, {
+        name: 'about',
+        url: '/about',
+        templateUrl: '/templates/about'
+      }
+    ];
 
-    appRun.$inject = [];
-    function appRun() {
-    }
+    states.forEach(function(state) {
+      $stateProvider.state(state);
+    });
+  }
+
+  appRun.$inject = [];
+  function appRun() {}
 })();
